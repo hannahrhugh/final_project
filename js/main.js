75411226053a28a3bfdf7a7ce74615105e591017
hannahrhugh <30412121+hannahrhugh@users.noreply.github.com>
2017-11-08 20:50:10 +0000

    styling changes

--- js/main.js
+++ js/main.js
@@ -1,42 +1,15 @@
-  $(document).ready(function(){
-      $('.slider-section').slick({
-        setting-name: setting-value
-      });
+$(document).ready(function(){
+  $('.slider-section').slick();
 
-      alert('hi)')
+  $('.hamburger').on('click',function(){
+    console.log('hamburger');
+
+    $('nav ul').toggleClass('show-nav')
+
+  }); 
 
 
-      $('.responsive').slick({
-  dots: true,
-  infinite: false,
-  speed: 300,
-  slidesToShow: 4,
-  slidesToScroll: 4,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-  ]
+
+
 
 });
-    });
